a6a5f2125b3f702159db32980b84fe6af0130d9f
Tobias Agyasta <agyasta1808@gmail.com>
2024-03-01 18:54:08 +0700

    Added light/dark mode and html-css carousel

--- script.js
+++ script.js
@@ -19,5 +19,30 @@ const observer_text = new IntersectionObserver(entries => {
         });
         });
     const image = document.querySelectorAll(".animated-image");
-    console.log(image);
     image.forEach((element) => observer_image.observe(element));
+
+
+    if (localStorage.getItem("darkMode") === "true") 
+        {
+            document.getElementById("switch-btn").checked = true;
+            document.body.classList.add("dark-mode");
+            document.getElementById('logo-image').src  = "assets/news-items/Black and White Modern Vintage Badge Brand Logo (dark-mode).png";
+            console.log('Dark')
+        }
+    else
+        {
+            document.getElementById('logo-image').src  = "assets/news-items/Black and White Modern Vintage Badge Brand Logo.png";
+        }
+    function toggleDarkMode() 
+        {
+            document.body.classList.toggle("dark-mode");
+            if (!document.getElementById("switch-btn").checked) 
+        {
+            document.getElementById('logo-image').src  = "assets/news-items/Black and White Modern Vintage Badge Brand Logo.png";
+            localStorage.setItem("darkMode", false);
+        } 
+        else {
+            document.getElementById('logo-image').src  = "assets/news-items/Black and White Modern Vintage Badge Brand Logo (dark-mode).png";
+            localStorage.setItem("darkMode", true);
+        }
+        }
